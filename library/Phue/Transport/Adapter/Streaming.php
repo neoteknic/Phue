64055071c462ad7950c583b8069743e2587a5055
Christopher Georg <christopher.georg@sr-travel.de>
2024-04-12 15:20:42 +0200

    feat: drop support for PHP < 8.1 bump min versions migrate to PHPUnit 11 fix ci deprecations

--- library/Phue/Transport/Adapter/Streaming.php
+++ library/Phue/Transport/Adapter/Streaming.php
@@ -68,8 +68,12 @@ class Streaming implements AdapterInterface
     public function getHttpStatusCode(): int
     {
         preg_match('#^HTTP/1\.1 (\d+)#mi', $this->getHeaders(), $matches);
-        
-        return $matches[1] ?? false;
+
+        if(isset($matches[1])) {
+            return (int) $matches[1];
+        }
+
+        return 500;
     }
 
     /**
