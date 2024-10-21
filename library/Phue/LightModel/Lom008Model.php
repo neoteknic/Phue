187ab2994fdd4d25ab71d0169f02395483d26c56
Pierre-Emmanuel REMY <pe@bwagence.fr>
2024-10-21 21:28:38 +0200

    Add models

---
+++ library/Phue/LightModel/Lom008Model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Phue: Philips Hue PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2012 Michael K. Squires
+ * @license   http://github.com/sqmk/Phue/wiki/License
+ */
+namespace Phue\LightModel;
+
+/**
+ * Hue BR30
+ */
+class Lom008Model extends AbstractLightModel
+{
+
+    /**
+     * Model id
+     */
+    const MODEL_ID = 'LOM008';
+
+    /**
+     * Model name
+     */
+    const MODEL_NAME = 'Hue Smart Plug';
+}
