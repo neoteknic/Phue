a2e98166f47aba4698b145918a15b0f511d7bbfc
Neoteknic <pe@bwagence.fr>
2024-10-22 23:35:06 +0200

    Add lamp model

---
+++ library/Phue/LightModel/L1746230p7Model.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Phue: Philips Hue PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2012 Michael K. Squires
+ * @license   http://github.com/sqmk/Phue/wiki/License
+ */
+namespace Phue\LightModel;
+
+/**
+ * Hue LightStrips
+ */
+class L1746230p7Model extends AbstractLightModel
+{
+
+    /**
+     * Model id
+     */
+    const MODEL_ID = '1746230P7';
+
+    /**
+     * Model name
+     */
+    const MODEL_NAME = 'Hue Lily XL outdoor color spot';
+
+    /**
+     * Can retain state
+     */
+    const CAN_RETAIN_STATE = true;
+}
